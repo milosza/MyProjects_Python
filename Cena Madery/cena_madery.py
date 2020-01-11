d392ae4357655aeb734f81e44b5a81a6355bab76
Milosz A <milosza@users.noreply.github.com>
2020-01-11 19:36:36 +0100

    script autorun added

--- Cena Madery/cena_madery.py
+++ Cena Madery/cena_madery.py
@@ -13,32 +13,34 @@ while True:
         # ścieżka XPATH dla ceny
         xpath_cena = '//*[@id="content"]/main/div[1]/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div/div/span/text()'
 
+        print('=== START', time.ctime(time.time()))
+
         # otwieram przegladarke i pobieram stronę
         driver = webdriver.Firefox()
         driver.get(url_tui1)
         # driver.maximize_window()
-        print(url_tui1)
+        print('URL:', url_tui1)
 
         # czekam na załadowanie strony
-        time.sleep(20)
+        time.sleep(120)
 
         # pobieram i parsuje stronę
         strona = driver.page_source
         tree = html.fromstring(driver.page_source)
 
-        # wyszukuje interesujący nas element i oczyszczam dane
+        # wyszukuje interesujący nas element i pobieram czas
         cena = tree.xpath(xpath_cena)
+        dzis = date.today()
+
+        # oczyszczam i przygotowuje dane
         cena = [word.replace(' ', '') for word in cena]
         cena = int(cena[0])
-        print(cena)
+        data = dzis.strftime("%Y-%m-%d")
+        print('Uzyskana cena:', cena)
+        print('Data ceny:', data)
 
         # zamykam przegladarke
         driver.quit()
-
-        # ustawiam date wykonania skryptu
-        dzis = date.today()
-        data = dzis.strftime("%Y-%m-%d")
-        print(data)
 
         # zapisuje dane do xls
         print("Zapisuję do xlsx...")
@@ -47,16 +49,20 @@ while True:
         sheet.append([data, cena, url_tui1])
         file.save('.\madera2020.xlsx')
         file.close()
-        print("OK. Naciśnij cokolwiek, aby wyjść.")
+        print("Zapisane. Za 24h uruchomię się ponownie.")
+        print('=== KONIEC', time.ctime(time.time()))
+        print()
+        time.sleep(60*60*24)
     except Exception as error:
         # w przypadku błędu, wypisuje błąd
+        print('=== BLAD', time.ctime(time.time()))
         print(error)
         # zamykam przegladarke
         driver.quit()
         # czekam na kolejne wywolanie skryptu
-        timeout = randint(1, 30)
+        timeout = randint(120, 240)
         print("Spróbuję ponownie za", timeout, "sekund...")
         time.sleep(timeout)
         pass
     else:
-        break
+        pass
